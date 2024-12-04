d608c0ff5a1f78aaf966733c045a33cae27cf139
Harmanpreet Singh <118050132+Harmanpreet-11@users.noreply.github.com>
2024-12-04 04:48:49 +0000

    commit changes done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+// let score = "true"
+// console.log(typeof score);
+
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);  // (NaN) Not a number
+
+
+//  "33" easily convertable
+//  "33abc" not convertable - NaN
+//  true -> 1 | false -> 0
+
+
+let isLoggedIn =  1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//  1 -> true | 0-> false
+// "" empty string false
+//  "harman" -> true 
