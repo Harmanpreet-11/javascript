334ac76cf9a4fc32e50504565a015547b8f46d74
Harmanpreet Singh <118050132+Harmanpreet-11@users.noreply.github.com>
2024-12-05 03:40:59 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); // false 
+// console.log(null == 0); // false
+// console.log(null >= 0); // ( true ) In this case the null is converted to 0 and thus predictable output is not given 
+
+// null convert into either NaN or 0 // avoid these conversion
+
+// console.log(undefined > 0); // false    ``
+// console.log(undefined == 0); // false     | avoid these comparison
+// console.log(undefined >= 0); // false   ``
+
+// === // Checks for datatype also 
+
+console.log("2" === 2);
