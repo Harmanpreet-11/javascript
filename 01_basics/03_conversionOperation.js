6b7343a5d92e90963fb29475b3522136ddf6e95c
Harmanpreet Singh <118050132+Harmanpreet-11@users.noreply.github.com>
2024-12-05 03:29:59 +0000

    Conversion Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,11 +11,47 @@
 //  true -> 1 | false -> 0
 
 
-let isLoggedIn =  1
+// let isLoggedIn =  1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 //  1 -> true | 0-> false
 // "" empty string false
 //  "harman" -> true 
+
+// *********************** Operations ***********************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+// let str1 = "Hello"
+// let str2 = " harman"
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + "2" + 2);
+
+// console.log(true) // op - true
+// console.log(+""); // op - 0
+
+
+// let num1 , num2 , num3
+// num1 = num2 = num3 = 2+2
+// console.log(num1)
+// console.log(num2);
+// console.log(num3);
+
+let gameCounter = 100
+
+++gameCounter;
+console.log(gameCounter);
