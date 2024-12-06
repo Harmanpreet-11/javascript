cb33d7e666e85209efa71c07c59a9edbb4126272
Harmanpreet Singh <118050132+Harmanpreet-11@users.noreply.github.com>
2024-12-06 15:23:33 +0000

    number and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+// const score = 400 
+
+// console.log(score);
+
+// const balance = new Number(150)
+
+// console.log(balance);
+// console.log(balance.toFixed(2)) // for precision value in long calculations 
+
+// const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(3)) // priroity is given before decimal 
+
+// const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// *********************MATHS*********************
+
+//library used in javascript 
+
+
+// console.log(Math);
+// console.log(Math.abs(-1));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(3.5));
+// console.log(Math.floor(4.6));
+
+// console.log(Math.random()*10); // random values 
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * max - min + 1)) + min)  // range depend on max and min this is the formula . Here we are getting any random number between 11 - 20
+
