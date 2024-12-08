7d347e9db3e888ba949cecd6bb89f59ea2cc5a9a
Harmanpreet Singh <118050132+Harmanpreet-11@users.noreply.github.com>
2024-12-08 08:09:07 +0000

    arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,24 @@
+// const marvel_heros = ["thor", "ironman" , "spiderman"]
+// const dc_heroes = ["superman" , "deadpool" , "batman"]
+
+// heroes = marvel_heros.concat(dc_heroes)
+
+// console.log(heroes);
+
+//  const all_new_heroes = [...marvel_heros, ...dc_heroes]
+//  console.log(all_new_heroes);
+ 
+//  const another_array = [1,2,3 , [4,5,6] ,7 ,[6,7 , [4,5]]]
+
+//  const real_another_array = another_array.flat(2)
+//  console.log(real_another_array);
+ 
+
+ console.log(Array.from("12"))
+
+
+ let score1 = 100
+ let score2 = 200
+ let score3 = 300
+
+ console.log(Array.of(score1,score2,score3))
